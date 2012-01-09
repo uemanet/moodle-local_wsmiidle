ca2ad5c2b52381939f5af354a337c0812de515ed
Jerome Mouneyrac <jerome@moodle.com>
2012-01-09 15:33:49 +0800

    Add version maturity information

--- version.php
+++ version.php
@@ -21,3 +21,4 @@ $plugin->version  = 2011101202;   // The (date) version of this module + 2 extra
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
 $plugin->cron     = 0;
 $plugin->release = '1.0 (Build: 2011101202)';
+$plugin->maturity = MATURITY_STABLE;
