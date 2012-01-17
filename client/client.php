f9785ec4593e6490db15ca06904b4687cb231539
Jerome Mouneyrac <jerome@moodle.com>
2012-01-17 12:00:05 +0800

    MDL-31138 mention which service to pick when creating a token

--- client/client.php
+++ client/client.php
@@ -18,7 +18,7 @@
 // 1- Install the plugin
 // 2- Enable web service advance feature (Admin > Advanced features)
 // 3- Enable XMLRPC protocol (Admin > Plugins > Web services > Manage protocols)
-// 4- Create a token for a specific user (Admin > Plugins > Web services > Manage tokens)
+// 4- Create a token for a specific user and for the service 'My service' (Admin > Plugins > Web services > Manage tokens)
 // 5- Run this script directly from your browser: you should see 'Hello, FIRSTNAME'
 
 /// SETUP - NEED TO BE CHANGED
